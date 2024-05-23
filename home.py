52abf44bf821b5460579727ec75df2b66f7ee024
benmo1221 <114561467+benmo1221@users.noreply.github.com>
2024-05-23 13:58:43 +0300

    Update home.py

--- home.py
+++ home.py
@@ -1,11 +1,20 @@
-import streamlit as st 
+import streamlit as st
+import sys
+import os
 
+# Dynamically add the yolov5 directory to sys.path
+yolov5_path = os.path.join(os.path.dirname(__file__), 'yolov5')
+if yolov5_path not in sys.path:
+    sys.path.append(yolov5_path)
+
+# Set Streamlit page configuration
 st.set_page_config(page_title="Home",
                    layout='wide',
                    page_icon='./images/home.png')
 
+# Page title and caption
 st.title("YOLO V5 Object Detection App")
-st.caption('This web application demostrate Object Detection')
+st.caption('This web application demonstrates Object Detection')
 
 # Content
 st.markdown("""
@@ -13,11 +22,8 @@ st.markdown("""
 - [Click here for Image Detection](/Image_detection/)  
 - [Click here for Real Time Detection](/Real_time_detection/)  
 
-the app detects 3 objects: 
+The app detects 3 objects: 
 1. gun - רובה ארוך
 2. knife - סכין
-3. pistol  - אקדח
-
-
-
+3. pistol - אקדח
 """)
