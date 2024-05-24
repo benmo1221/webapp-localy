1e1ed6ef5ab0e11ed1e5099216f25bc00d4e797c
benmo1221 <114561467+benmo1221@users.noreply.github.com>
2024-05-25 00:38:26 +0300

    Update video_detection.py

--- pages/video_detection.py
+++ pages/video_detection.py
@@ -9,7 +9,7 @@ import os
 
 
 # Dynamically add the yolov5 directory to sys.path
-yolov5_path = os.path.join(os.path.dirname(__file__), 'yolov5')
+yolov5_path = os.path.join(os.path.dirname(__file__), '..', 'yolov5')
 if yolov5_path not in sys.path:
     sys.path.append(yolov5_path)
 
